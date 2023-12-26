d7254a66fbba1b164050a829060971bf4d5e47a2
jamshaidkhalid <jamshaidkhalid32@gmail.com>
2023-12-26 10:25:29 +0500

    multiple instances of server

--- server/index.js
+++ server/index.js
@@ -29,7 +29,8 @@ const environment = process.env.NODE_ENV;
 
 if (environment === 'development') {
 
-    const PORT = process.env.PORT || 5000;
+    const defaultPort = 5000;
+    const PORT = process.argv[2] || process.env.PORT || defaultPort;
 
     app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
